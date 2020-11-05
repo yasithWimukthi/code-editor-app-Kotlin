1214946516e536f51f697a7c0e3f182b392be432
yasithWimukthi <it19966922@my.sliit.lk>
2020-11-06 00:10:24 +0530

    modify code activity

--- app/src/main/java/com/androidstudio/kotlin/codeeditor/code_activity.kt
+++ app/src/main/java/com/androidstudio/kotlin/codeeditor/code_activity.kt
@@ -2,10 +2,18 @@ package com.androidstudio.kotlin.codeeditor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class code_activity : AppCompatActivity() {
+
+    lateinit var web_browser:WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_code_activity)
+
+        web_browser = findViewById(R.id.web_browser)
+
+        web_browser.loadData(intent.getStringExtra("code").toString(),"text/html; charset=utf8",null)
     }
 }
