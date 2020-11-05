03f2c7c017718d3004666a59e4681ff0893b401e
yasithWimukthi <it19966922@my.sliit.lk>
2020-11-05 23:41:42 +0530

    modify main activity

--- app/src/main/java/com/androidstudio/kotlin/codeeditor/MainActivity.kt
+++ app/src/main/java/com/androidstudio/kotlin/codeeditor/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.androidstudio.kotlin.codeeditor
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AutoCompleteTextView
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var user_code:AutoCompleteTextView
+    lateinit var run_btn:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        user_code = findViewById(R.id.user_code)
+        run_btn = findViewById(R.id.run_btn)
+
+        run_btn.setOnClickListener{
+            Run()
+        }
+    }
+
+    private fun Run() {
+        var intent = Intent(this@MainActivity,code_activity::class.java)
+        intent.putExtra("code",user_code.text.toString())
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/androidstudio/kotlin/codeeditor/code_activity.kt
@@ -0,0 +1,11 @@
+package com.androidstudio.kotlin.codeeditor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class code_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_code_activity)
+    }
+}
